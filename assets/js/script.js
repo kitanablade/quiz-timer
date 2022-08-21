dfc39d051bab000683fa4f4f40a3b5968c9bec9b
kitanablade <kiteagle@gmail.com>
2022-08-21 16:41:35 -0700

    working timer, static question display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,82 @@
-const questions = [
-    "How long did it take for the astronauts to get to the moon?",
-    "How far is the moon from the earth?",
-    "Who was the first person to set foot on the moon?",
-    "What year did the first moon landing occur?",
-    "How tall was the Saturn V moon rocket?"
-]
-let curentQuestion = 0;
+let currentQuestion = 1;
 let timeLeft = 30;
 let score = 0;
 let highscores = 0;
+let finished = false;
 
+const startBtn = document.querySelector(".start-button")
+const timerElement = document.querySelector(".timer-count");
+const question = document.getElementById("question");
+const answerBtn1 = document.getElementById("answer-btn-1");
+const answerBtn2 = document.getElementById("answer-btn-2");
+const answerBtn3 = document.getElementById("answer-btn-3");
+const answerBtn4 = document.getElementById("answer-btn-4");
+
+const quesAnsBank = [
+  {
+    question: "How long did it take for the astronauts to get to the moon?",
+    answers: {
+      answer1: "4 hours",
+      answer2: "4 days",
+      answer3: "4 weeks",
+      answer4: "1 week",
+    },
+    correct: "answer2",
+  },
+  {
+    question: "How far is the moon from the earth?",
+    answers: {
+      answer1: "239,000 mi",
+      answer2: "239,000 km",
+      answer3: "239 miles",
+      answer4: "239,000 feet",
+    },
+    correct: "answer1",
+  },
+  {
+    question: "Who was the second person to set foot on the moon?",
+    answers: {
+      answer1: "John Glenn",
+      answer3: "Buzz Lightyear",
+      answer2: "Alan Shepard",
+      answer4: "Buzz Aldrin",
+    },
+    correct: "answer4"
+  },
+  //    { "Who was the first person to set foot on the moon?"},
+  //     "Who was the second person to set foot on the moon?",
+  //     "How tall was the Saturn V moon rocket?"
+];
+
+function loadquestion(currentQuestion){
+    question.textContent = quesAnsBank[currentQuestion].question;
+    answerBtn1.value = quesAnsBank[currentQuestion].answers.answer1;
+    answerBtn2.value = quesAnsBank[currentQuestion].answers.answer2;
+    answerBtn3.value = quesAnsBank[currentQuestion].answers.answer3;
+    answerBtn4.value = quesAnsBank[currentQuestion].answers.answer4;
+    }
+
+    //loadquestion(currentQuestion);
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timeLeft--;
+      timerElement.textContent = timeLeft;
+    //   if (timeLeft >= 0) {
+    //     // Tests if win condition is met
+    //     if (finished && timeLeft > 0) {
+    //       // Clears interval and stops timer
+    //       clearInterval(timer);
+    //       //winGame();
+    //     }
+    //   }
+      // Tests if time has run out
+      if (timeLeft === 0) {
+        // Clears interval
+        clearInterval(timer);
+        //loseGame();
+      }
+    }, 1000);
+  }
+  startTimer();
