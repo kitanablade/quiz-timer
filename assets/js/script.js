0b8e1244a6dc81d532b7492ebeddcd444a03c60f
kitanablade <kiteagle@gmail.com>
2022-08-21 12:01:20 -0700

    pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const questions = [
+    "How long did it take for the astronauts to get to the moon?",
+    "How far is the moon from the earth?",
+    "Who was the first person to set foot on the moon?",
+    "What year did the first moon landing occur?",
+    "How tall was the Saturn V moon rocket?"
+]
+let curentQuestion = 0;
+let timeLeft = 30;
+let score = 0;
+let highscores = 0;
+
