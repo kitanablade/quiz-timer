9a7bdaf264033645af4162b3f56eb30530023f23
kitanablade <kiteagle@gmail.com>
2022-08-21 20:42:15 -0700

    fully functional app; no styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,24 @@ let timeLeft = 30;
 let score = 0;
 let highscores = 0;
 let finished = false;
-answeredQs = 0;
 
-const startBtn = document.querySelector(".start-button")
+const startBtn = document.querySelector(".start-button");
 const timerElement = document.querySelector(".timer-count");
 const question = document.getElementById("question");
 const answerBtn = document.getElementsByClassName(".answer-buttons");
 const statusBoard = document.querySelector(".game-status");
+const scoreDisp = document.querySelector(".score");
 
 const answerBtn1 = document.getElementById("answer-btn-1");
 const answerBtn2 = document.getElementById("answer-btn-2");
 const answerBtn3 = document.getElementById("answer-btn-3");
 const answerBtn4 = document.getElementById("answer-btn-4");
 
-function init(){
-    answerBtn1.disabled = true;
-    answerBtn2.disabled = true;
-    answerBtn3.disabled = true;
-    answerBtn4.disabled = true;
+function init() {
+  answerBtn1.disabled = true;
+  answerBtn2.disabled = true;
+  answerBtn3.disabled = true;
+  answerBtn4.disabled = true;
 }
 
 const quesAnsBank = [
@@ -32,7 +32,7 @@ const quesAnsBank = [
       answer3: "4 weeks",
       answer4: "1 week",
     },
-    correct: "4 days"
+    correct: "4 days",
   },
   {
     question: "How far is the moon from the earth?",
@@ -52,7 +52,7 @@ const quesAnsBank = [
       answer2: "Alan Shepard",
       answer4: "Buzz Aldrin",
     },
-    correct: "Neil Armstrong"
+    correct: "Neil Armstrong",
   },
   {
     question: "Who was the second person to set foot on the moon?",
@@ -62,7 +62,7 @@ const quesAnsBank = [
       answer2: "Alan Shepard",
       answer4: "Buzz Aldrin",
     },
-    correct: "Buzz Aldrin"
+    correct: "Buzz Aldrin",
   },
   {
     question: "How tall was the Saturn V moon rocket?",
@@ -72,7 +72,7 @@ const quesAnsBank = [
       answer2: "363 feet",
       answer4: "663 ft",
     },
-    correct: "363 feet"
+    correct: "363 feet",
   },
 ];
 
@@ -82,66 +82,74 @@ startBtn.addEventListener("click", startGame);
 // answerBtn3.addEventListener("click", checkAnswer);
 // answerBtn4.addEventListener("click", checkAnswer);
 
-function loadquestion(){
-    
-    question.textContent = quesAnsBank[currentQuestionIndex].question;
-    answerBtn1.value = quesAnsBank[currentQuestionIndex].answers.answer1;
-    answerBtn2.value = quesAnsBank[currentQuestionIndex].answers.answer2;
-    answerBtn3.value = quesAnsBank[currentQuestionIndex].answers.answer3;
-    answerBtn4.value = quesAnsBank[currentQuestionIndex].answers.answer4;
-    
-    }
+function loadquestion() {
+  question.textContent = quesAnsBank[currentQuestionIndex].question;
+  answerBtn1.value = quesAnsBank[currentQuestionIndex].answers.answer1;
+  answerBtn2.value = quesAnsBank[currentQuestionIndex].answers.answer2;
+  answerBtn3.value = quesAnsBank[currentQuestionIndex].answers.answer3;
+  answerBtn4.value = quesAnsBank[currentQuestionIndex].answers.answer4;
+}
 
-    // The startGame function is called when the start button is clicked
+// The startGame function is called when the start button is clicked
 function startGame() {
-    answerBtn1.disabled = false;
-    answerBtn2.disabled = false;
-    answerBtn3.disabled = false;
-    answerBtn4.disabled = false;
-    loadquestion();
-    // Prevents start button from being clicked when round is in progress
-    startBtn.disabled = true;
-    startTimer()
-  }
+  answerBtn1.disabled = false;
+  answerBtn2.disabled = false;
+  answerBtn3.disabled = false;
+  answerBtn4.disabled = false;
+  loadquestion();
+  // Prevents start button from being clicked when round is in progress
+  startBtn.disabled = true;
+  startTimer();
+}
 // The setTimer function starts and stops the timer and triggers the end of the quiz
 function startTimer() {
-    // Sets timer
-    timer = setInterval(function() {
-      timeLeft--;
-      timerElement.textContent = timeLeft;
-      if (timeLeft >= 0) {
-        // Tests if all questions have been answered
-        if (finished && timeLeft > 0) {
-          // Clears interval and stops timer
-          clearInterval(timer);
-          //finishGame();
-        }
-      }
-      // Tests if time has run out
-      if (timeLeft === 0) {
-        // Clears interval
+  // Sets timer
+  timer = setInterval(function () {
+    timeLeft--;
+    timerElement.textContent = (`:${timeLeft}`);
+    if (timeLeft >= 0) {
+      // Tests if all questions have been answered
+      if (finished && timeLeft > 0) {
+        // Clears interval and stops timer
         clearInterval(timer);
-        //loseGame();
+        timerElement.textContent = ":00";
+        endGame();
       }
-    }, 1000);
-  }
-  
-  function checkAnswer(answer){
-    // if (answeredQs == quesAnsBank.length){
-    //     return finished = true; 
-    // }else{
-    //     loadquestion();
-    // }
-    //currentQuestionIndex++;
-    console.log(answer.value);
-    if (answer.value == quesAnsBank[currentQuestionIndex].correct){
-        console.log("CORRECT!");
-        statusBoard.textContent = "CORRECT!"
-    }else{
-        console.log("WRONG!!");
-        statusBoard.textContent = "WRONG"
     }
-    //console.log(quesAnsBank[currentQuestionIndex].correct);
-  }
+    // Tests if time has run out
+    if (timeLeft === 0) {
+      // Clears interval
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+}
 
-  init();
+function checkAnswer(answer) {
+  console.log(answer.value);
+  if (answer.value == quesAnsBank[currentQuestionIndex].correct) {
+    statusBoard.textContent = "🧑‍🚀CORRECT!";
+    score++;
+    scoreDisp.textContent = score;
+  } else {
+    statusBoard.textContent = "Sorry, that's wrong.";
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex == quesAnsBank.length) {
+    finished = true;
+    endGame();
+  } else {
+    loadquestion();
+  }
+}
+
+function endGame() {
+
+  answerBtn1.disabled = true;
+  answerBtn2.disabled = true;
+  answerBtn3.disabled = true;
+  answerBtn4.disabled = true;
+  statusBoard.textContent = "🌕 MISSION COMPLETE 🌕"
+}
+
+init();
